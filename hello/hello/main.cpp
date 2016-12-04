197c9a6e2019c0fa306e34497580ae5060c6c470
unknown <>
2016-12-04 23:38:09 +0900

    STL Practice ver 2

--- hello/hello/main.cpp
+++ hello/hello/main.cpp
@@ -1,5 +1,9 @@
 #include <process.h>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
 #include "KeyEvent.h"
 #include "ExArray.h"
 #include "Frame.h"
@@ -33,6 +37,15 @@ unsigned int __stdcall keyEvent(void*){
 	return 0;
 }
 
+void dump(list<string> &l){	//	print data of list.
+	list<string>::iterator iter = l.begin();
+
+	while(iter != l.end()){
+		cout << *iter << endl;
+		iter++;
+	}
+}
+
 int main(){
 	/*
 	HANDLE handleA, handleB;
@@ -45,6 +58,7 @@ int main(){
 
 	CloseHandle(handleA);
 	CloseHandle(handleB);*/
+	/*
 	ExArray<int> arr1(3);
 	ExArray<char> arr2(4);
 	ExArray<double> arr3(5);
@@ -74,5 +88,53 @@ int main(){
 	arr1.printData();
 	arr4.printData();
 	
+	return 0;*/
+	//	vector
+	vector<int> v;
+
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.insert(v.begin(), 40);
+	v.insert(v.end(), 50);
+
+	for(int i = 0; i < v.size(); i++){
+		cout << v[i] << endl;
+	}
+
+	cout << "access index at 1 : " << v.at(1) << endl;
+
+	v.erase(v.begin());
+	v.erase(v.end() - 1);
+
+	vector<int>::iterator iter = v.begin();
+
+	cout << "access index at 1 with iterator : " << iter[1] << endl;
+	
+	while(iter != v.end()){
+		cout << *iter << endl;
+		iter++;
+	}
+	cout << endl;
+	//	list
+	list<string> names;
+	names.insert(names.begin(), "Konkuk");
+	names.insert(names.end(), "University");
+	names.insert(names.end(), "SCLAB");
+	names.insert(names.end(), "CSY");
+	dump(names);
+	cout << "===" << endl;
+	names.reverse();
+	dump(names);
+	cout << endl;
+	//	map
+	map<string, int> m;
+	m["Seoul"] = 100;
+	m["Daegu"] = 120;
+	m["Busan"] = 200;
+	
+	cout << "Train to Seoul = " << m["Seoul"] << "$"  << endl;
+	cout << "Train to Daegu = " << m["Daegu"] << "$" << endl;
+	cout << "Train to Busan = " << m["Busan"] << "$" << endl;
 	return 0;
 }
